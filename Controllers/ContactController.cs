79e9109bb5950150750fb22c7865cc71a3ddfc9a
AlexShirin <aldorian@mail.ru>
2024-01-08 07:51:01 +0300

    refactoring

--- Controllers/ContactController.cs
+++ Controllers/ContactController.cs
@@ -13,12 +13,10 @@ namespace ContactManagerCS.Controllers;
 [Produces("application/json")]
 public class ContactController : ControllerBase
 {
-    //private readonly ContactDbContext _context;
     private IContactRepository _repo;
 
     public ContactController(ContactDbContext context, IContactRepository repo)
     {
-        //_context = context;
         _repo = repo;
     }
 
@@ -28,7 +26,6 @@ public class ContactController : ControllerBase
     [HttpGet]
     public async Task<List<Contact>> GetAll()
     {
-        //return await _context.ContactItems.ToListAsync();
         return await _repo.GetAll();
     }
 
@@ -38,8 +35,6 @@ public class ContactController : ControllerBase
     [HttpGet("{id}")]
     public async Task<ActionResult<Contact>> GetById(int id)
     {
-        //var item = await _context.ContactItems.FindAsync(id);
-        //return item is null ? NotFound() : item;
         return await _repo.GetById(id);
     }
 
@@ -49,13 +44,6 @@ public class ContactController : ControllerBase
     [HttpPost]
     public async Task<ActionResult<Contact>> Create(Contact item)
     {
-        //var exists = await _context.ContactItems.FindAsync(item.Id);
-        //if (exists is not null) { return BadRequest("Contact with given Id already exists"); }
-
-        //_context.ContactItems.Add(item);
-        //await _context.SaveChangesAsync();
-
-        //return item;
         return await _repo.Create(item);
     }
 
@@ -65,13 +53,6 @@ public class ContactController : ControllerBase
     [HttpPut]
     public async Task<ActionResult<Contact>> Update(Contact item)
     {
-        //var exists = await _context.ContactItems.FindAsync(item.Id);
-        //if (exists is null) { return BadRequest("Contact with given Id don't exist"); }
-
-        //_context.ContactItems.Add(item);
-        //await _context.SaveChangesAsync();
-
-        //return item;
         return await _repo.Update(item);
     }
 
@@ -81,13 +62,6 @@ public class ContactController : ControllerBase
     [HttpDelete("{id}")]
     public async Task<ActionResult<Contact>> DeleteById(int id)
     {
-        //var item = await _context.ContactItems.FindAsync(id);
-        //if (item is null) { return BadRequest("Contact with given Id don't exist"); }
-
-        //_context.ContactItems.Remove(item);
-        //await _context.SaveChangesAsync();
-
-        //return item;
         return await _repo.DeleteById(id);
     }
 }
