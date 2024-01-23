a04a2f4d1a2ffd1006dca37763b82cb6e2bb19f0
AlexShirin <aldorian@mail.ru>
2024-01-23 09:58:16 +0300

    total test rework

--- Tests/ContactServiceTests.cs
+++ Tests/ContactServiceTests.cs
@@ -1,12 +1,18 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+
 using AutoMapper;
+
 using ContactManagerCS.Contracts;
 using ContactManagerCS.Exceptions;
 using ContactManagerCS.Models;
 using ContactManagerCS.Services;
 using ContactManagerCS.Tests.Helpers;
+
+using FluentValidation;
+
 using Moq;
 
 namespace ContactManagerCS.Tests;
@@ -17,16 +23,16 @@ public class ContactServiceTests
     private readonly ContactService _contactService;
     private readonly IMapper _mapper;
 
-    private List<Contact> _listContacts;
-    private List<ContactResponse> _listContactResponses;
+    //private List<Contact> _listContacts;
+    //private List<ContactResponse> _listContactResponses;
 
-    private int _id;
-    private Contact _contact4;
-    private AddContactRequest _contact4req;
-    private ContactResponse _contact4res;
-    private Contact _contact4u;
-    private AddContactRequest _contact4ureq;
-    private ContactResponse _contact4ures;
+    //private int _id;
+    //private Contact _contact4;
+    //private AddContactRequest _contact4req;
+    //private ContactResponse _contact4res;
+    //private Contact _contact4u;
+    //private AddContactRequest _contact4ureq;
+    //private ContactResponse _contact4ures;
 
     public ContactServiceTests()
     {
@@ -36,35 +42,34 @@ public class ContactServiceTests
         _mockRepo = new Mock<IContactRepository>();
         _contactService = new ContactService(_mockRepo.Object, _mapper);
 
-        _listContacts =
-        [
-            new Contact { Id = 1, Name = "Tom", Email = "a@a.a", Phone = "11", Work = "A" },
-            new Contact { Id = 2, Name = "Bob", Email = "b@a.a", Phone = "22", Work = "B" },
-            new Contact { Id = 3, Name = "Sam", Email = "c@a.a", Phone = "33", Work = "C" },
-        ];
+        //_listContacts =
+        //[
+        //    new Contact { Id = 1, Name = "Tom", Email = "a@a.a", Phone = "11", Work = "A" },
+        //    new Contact { Id = 2, Name = "Bob", Email = "b@a.a", Phone = "22", Work = "B" },
+        //    new Contact { Id = 3, Name = "Sam", Email = "c@a.a", Phone = "33", Work = "C" },
+        //];
 
-        _listContactResponses =
-        [
-            new ContactResponse { Id = 1, Name = "Tom", Email = "a@a.a", Phone = "11", Work = "A" },
-            new ContactResponse { Id = 2, Name = "Bob", Email = "b@a.a", Phone = "22", Work = "B" },
-            new ContactResponse { Id = 3, Name = "Sam", Email = "c@a.a", Phone = "33", Work = "C" },
-        ];
+        //_listContactResponses =
+        //[
+        //    new ContactResponse { Id = 1, Name = "Tom", Email = "a@a.a", Phone = "11", Work = "A" },
+        //    new ContactResponse { Id = 2, Name = "Bob", Email = "b@a.a", Phone = "22", Work = "B" },
+        //    new ContactResponse { Id = 3, Name = "Sam", Email = "c@a.a", Phone = "33", Work = "C" },
+        //];
 
-        _id = 4;
-        _contact4 = new Contact { Id = 4, Name = "d", Email = "d@d.d", Phone = "44", Work = "D" };
-        _contact4req = new AddContactRequest { Id = 4, Name = "d", Email = "d@d.d", Phone = "44", Work = "D" };
-        _contact4res = new ContactResponse { Id = 4, Name = "d", Email = "d@d.d", Phone = "44", Work = "D" };
-        _contact4u = new Contact { Id = 4, Name = "e", Email = "d@d.d", Phone = "44", Work = "D" };
-        _contact4ureq = new AddContactRequest { Id = 4, Name = "e", Email = "d@d.d", Phone = "44", Work = "D" };
-        _contact4ures = new ContactResponse { Id = 4, Name = "e", Email = "d@d.d", Phone = "44", Work = "D" };
+        //_id = 4;
+        //_contact4 = new Contact { Id = 4, Name = "d", Email = "d@d.d", Phone = "44", Work = "D" };
+        //_contact4req = new AddContactRequest { Id = 4, Name = "d", Email = "d@d.d", Phone = "44", Work = "D" };
+        //_contact4res = new ContactResponse { Id = 4, Name = "d", Email = "d@d.d", Phone = "44", Work = "D" };
+        //_contact4u = new Contact { Id = 4, Name = "e", Email = "d@d.d", Phone = "44", Work = "D" };
+        //_contact4ureq = new AddContactRequest { Id = 4, Name = "e", Email = "d@d.d", Phone = "44", Work = "D" };
+        //_contact4ures = new ContactResponse { Id = 4, Name = "e", Email = "d@d.d", Phone = "44", Work = "D" };
     }
 
-    [Theory]
-    [MemberData(nameof(ContactTestsHelper.GetAllTestData), MemberType = typeof(ContactTestsHelper))]
-    public async Task GetAllTest(List<Contact> listContacts)
+    [Fact]
+    public async Task GetAllTest()
     {
         //Arrange
-        _mockRepo.Setup(repo => repo.GetAll().Result).Returns(listContacts);
+        _mockRepo.Setup(repo => repo.GetAll().Result).Returns(ContactHelper.BaseContactList);
 
         //Act
         var result = await _contactService.GetAll();
@@ -72,15 +77,17 @@ public class ContactServiceTests
         //Assert
         Assert.NotNull(result);
         Assert.IsType<List<ContactResponse>>(result);
-        Assert.Equal(listContacts.Count, result.Count);
+        Assert.Equal(ContactHelper.BaseContactList.Count, result.Count);
     }
 
     [Theory]
-    [MemberData(nameof(ContactTestsHelper.GetByIdValidTestData), MemberType = typeof(ContactTestsHelper))]
-    public async Task GetByIdValidTest(int id, List<Contact> listContacts)
+    [InlineData(0)]
+    [InlineData(1)]
+    [InlineData(2)]
+    public async Task GetByIdValidTest(int id)
     {
         //Arrange
-        _mockRepo.Setup(repo => repo.GetById(id)).ReturnsAsync(listContacts.ElementAt(id - 1));
+        _mockRepo.Setup(repo => repo.GetById(id)).ReturnsAsync(ContactHelper.BaseContactList.ElementAt(id));
 
         //Act
         var result = await _contactService.GetById(id);
@@ -88,11 +95,12 @@ public class ContactServiceTests
         //Assert
         Assert.NotNull(result);
         Assert.IsType<ContactResponse>(result);
-        Assert.Equal(_listContactResponses.ElementAt(id - 1), result);
+        Assert.Equal(ContactHelper.BaseContactResponseList.ElementAt(id), result);
     }
 
     [Theory]
-    [MemberData(nameof(ContactTestsHelper.GetByIdNotValidTestData), MemberType = typeof(ContactTestsHelper))]
+    [InlineData(-1)]
+    [InlineData(3)]
     public async Task GetByIdNotValidTest(int id)
     {
         //Arrange
@@ -108,7 +116,93 @@ public class ContactServiceTests
     }
 
     [Theory]
-    [MemberData(nameof(ContactTestsHelper.CreateValidTestData), MemberType = typeof(ContactTestsHelper))]
+    [InlineData(null, "", "", "", "", false, false)]
+    [InlineData(1, null, "", "", "", false, false)]
+    [InlineData(1, "", null, "", "", false, false)]
+    [InlineData(1, "", "", null, "", false, false)]
+    [InlineData(1, "", "", "", null, false, false)]
+    [InlineData(2, "Jim", "d@d.d", "44", "D", true, true)]
+    [InlineData(3, "Jim", "d@d.d", "44", "D", true, false)]
+    public async Task CreateTest(int? id, string name, string email, string phone, string work, bool valid, bool exists)
+    {
+        //Arrange
+        var addContactRequest = new AddContactRequest { 
+            Id = id, Name = name, Email = email, Phone = phone, Work = work };
+        var addContact = new Contact { 
+            Id = (id == null ? 0 : id.Value), Name = name, Email = email, Phone = phone, Work = work };
+        var addContactResponse = new ContactResponse { 
+            Id = (id == null ? 0 : id.Value), Name = name, Email = email, Phone = phone, Work = work };
+        ContactResponse response = null;
+        ContactException exception = null;
+        //ValidationException validation = null;
+        Exception ex = null;
+
+        if (exists) 
+        {
+            _mockRepo.Setup(repo => repo.GetById(addContact.Id).Result).Returns(addContact);
+        }
+        else
+        {
+            _mockRepo.Setup(repo => repo.GetById(addContact.Id)).ReturnsAsync((Contact)null);
+        }
+        _mockRepo.Setup(repo => repo.Create(addContact).Result).Returns(addContact);
+
+        //Act
+        if (!valid)
+        {
+            ex = await Assert.ThrowsAsync<ValidationException>(() => _contactService.Create(addContactRequest));
+        }
+        else if (exists)
+        {
+            exception = await Assert.ThrowsAsync<ContactException>(() => _contactService.Create(addContactRequest));
+        }
+        else
+        {
+            response = await _contactService.Create(addContactRequest);
+        }
+
+        //Assert
+        if (!valid)
+        {
+            Assert.NotNull(ex);
+            Assert.IsType<ValidationException>(ex);
+            if (id == null)
+            {
+                Assert.Contains("Please specify a unique id", ex.Message);
+            }
+            else if (name == null)
+            {
+                Assert.Contains("Please specify a non-empty name", ex.Message);
+            }
+            else if (email == null)
+            {
+                Assert.Contains("Please specify a non-empty email", ex.Message);
+            }
+            else if (phone == null)
+            {
+                Assert.Contains("Please specify a non-empty phone", ex.Message);
+            }
+            else if (work == null)
+            {
+                Assert.Contains("Please specify a non-empty work name", ex.Message);
+            }
+        }
+        else if(exists)
+        {
+            Assert.NotNull(exception);
+            Assert.IsType<ContactException>(exception);
+            Assert.Contains("Can't Create: contact with given Id already exists", exception.Message);
+        }
+        else
+        {
+            Assert.NotNull(response);
+            Assert.IsType<ContactResponse>(response);
+            Assert.Equal(addContactResponse, response);
+        }
+    }
+
+    [Theory]
+    [MemberData(nameof(ContactHelper.CreateValidTestData), MemberType = typeof(ContactHelper))]
     public async Task CreateValidTest(Contact contact)
     {
         //Arrange
@@ -125,7 +219,7 @@ public class ContactServiceTests
     }
 
     [Theory]
-    [MemberData(nameof(ContactTestsHelper.CreateNotValidTestData), MemberType = typeof(ContactTestsHelper))]
+    [MemberData(nameof(ContactHelper.CreateNotValidTestData), MemberType = typeof(ContactHelper))]
     public async Task CreateNotValidTest(Contact contact)
     {
         //Arrange
@@ -143,7 +237,7 @@ public class ContactServiceTests
     }
 
     [Theory]
-    [MemberData(nameof(ContactTestsHelper.UpdateValidTestData), MemberType = typeof(ContactTestsHelper))]
+    [MemberData(nameof(ContactHelper.UpdateValidTestData), MemberType = typeof(ContactHelper))]
     public async Task UpdateValidTest(Contact contactToAdd, Contact contactToUpdate)
     {
         //Arrange
@@ -160,7 +254,7 @@ public class ContactServiceTests
     }
 
     [Theory]
-    [MemberData(nameof(ContactTestsHelper.UpdateNotValidTestData), MemberType = typeof(ContactTestsHelper))]
+    [MemberData(nameof(ContactHelper.UpdateNotValidTestData), MemberType = typeof(ContactHelper))]
     public async Task UpdateNotValidTest(Contact contactToAdd, Contact contactToUpdate)
     {
         //Arrange
@@ -178,7 +272,7 @@ public class ContactServiceTests
     }
 
     [Theory]
-    [MemberData(nameof(ContactTestsHelper.DeleteValidTestData), MemberType = typeof(ContactTestsHelper))]
+    [MemberData(nameof(ContactHelper.DeleteValidTestData), MemberType = typeof(ContactHelper))]
     public async Task DeleteValidTest(Contact contactToUpdate)
     {
         //Arrange
@@ -195,7 +289,7 @@ public class ContactServiceTests
     }
 
     [Theory]
-    [MemberData(nameof(ContactTestsHelper.DeleteNotValidTestData), MemberType = typeof(ContactTestsHelper))]
+    [MemberData(nameof(ContactHelper.DeleteNotValidTestData), MemberType = typeof(ContactHelper))]
     public async Task DeleteNotValidTest(Contact contactToUpdate)
     {
         //Arrange
@@ -210,21 +304,5 @@ public class ContactServiceTests
         Assert.NotNull(exception);
         Assert.IsType<ContactException>(exception);
         Assert.Contains("Can't Delete: contact with given Id don't exist", exception.Message);
-    }
-
-    [Fact]
-    public async Task AddContactRequestValidatorTest()
-    {
-        ////Arrange
-        //_mockRepo.Setup(repo => repo.GetById(_id)).ReturnsAsync((Contact)null);
-        //_mockRepo.Setup(repo => repo.Create(_contact4).Result).Returns(_contact4);
-
-        ////Act
-        //var result1 = await _contactService.Create(_contact4req);
-
-        ////Assert
-        //Assert.NotNull(result1);
-        //Assert.IsType<ContactResponse>(result1);
-        //Assert.Equal(_contact4res, result1);
     }
 }
---
+++ Tests/Helpers/ContactHelper.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using ContactManagerCS.Models;
+
+namespace ContactManagerCS.Tests.Helpers;
+
+internal static class ContactHelper
+{
+    public static List<Contact> BaseContactList => new()
+        {
+            new Contact { Id = 0, Name = "Tom", Email = "a@a.a", Phone = "11", Work = "A" },
+            new Contact { Id = 1, Name = "Bob", Email = "b@a.a", Phone = "22", Work = "B" },
+            new Contact { Id = 2, Name = "Sam", Email = "c@a.a", Phone = "33", Work = "C" },
+        };
+    public static List<ContactResponse> BaseContactResponseList => new()
+        {
+            new ContactResponse { Id = 0, Name = "Tom", Email = "a@a.a", Phone = "11", Work = "A" },
+            new ContactResponse { Id = 1, Name = "Bob", Email = "b@a.a", Phone = "22", Work = "B" },
+            new ContactResponse { Id = 2, Name = "Sam", Email = "c@a.a", Phone = "33", Work = "C" },
+        };
+    public static List<AddContactRequest> BaseAddContactRequestList => new()
+        {
+            new AddContactRequest { Id = 0, Name = "Tom", Email = "a@a.a", Phone = "11", Work = "A" },
+            new AddContactRequest { Id = 1, Name = "Bob", Email = "b@a.a", Phone = "22", Work = "B" },
+            new AddContactRequest { Id = 2, Name = "Sam", Email = "c@a.a", Phone = "33", Work = "C" },
+        };
+
+    public static object[] ValidContactIds = { 0, 1, 2 };
+    public static object[] NotValidContactIds = { -1, 3 };
+
+    public static Contact ContactToAdd = new() 
+        { Id = 3, Name = "Jim", Email = "d@d.d", Phone = "44", Work = "D" };
+    public static Contact ContactToUpdate = new() 
+        { Id = 3, Name = "Bim", Email = "d@d.d", Phone = "44", Work = "D" };
+
+
+    //public static IEnumerable<object[]> GetAllTestData()
+    //{
+    //    object[] tmp = [BaseContactList];
+    //    yield return tmp;
+    //}
+
+    //public static IEnumerable<object[]> GetByIdValidTestData()
+    //{
+    //    foreach (var contactId in ValidContactIds)
+    //    {
+    //        //object[] tmp = [contactId];
+    //        yield return [contactId, BaseContactList];
+    //    }
+    //}
+
+    //public static IEnumerable<object[]> GetByIdNotValidTestData()
+    //{
+    //    var allData = new List<object[]> { new object[] { NotValidContactIds.GetValue(0) } };
+    //    return allData.Take(1);
+    //}
+
+    public static IEnumerable<object[]> CreateValidTestData()
+    {
+        var allData = new List<object[]> { new object[] { ContactToAdd } };
+        return allData.Take(1);
+    }
+
+    public static IEnumerable<object[]> CreateNotValidTestData()
+    {
+        var allData = new List<object[]> { new object[] { ContactToAdd } };
+        return allData.Take(1);
+    }
+
+    public static IEnumerable<object[]> UpdateValidTestData()
+    {
+        var allData = new List<object[]> { new object[] { ContactToAdd, ContactToUpdate } };
+        return allData.Take(1);
+    }
+
+    public static IEnumerable<object[]> UpdateNotValidTestData()
+    {
+        var allData = new List<object[]> { new object[] { ContactToAdd, ContactToUpdate } };
+        return allData.Take(1);
+    }
+
+    public static IEnumerable<object[]> DeleteValidTestData()
+    {
+        var allData = new List<object[]> { new object[] { ContactToUpdate } };
+        return allData.Take(1);
+    }
+
+    public static IEnumerable<object[]> DeleteNotValidTestData()
+    {
+        var allData = new List<object[]> { new object[] { ContactToUpdate } };
+        return allData.Take(1);
+    }
+}
