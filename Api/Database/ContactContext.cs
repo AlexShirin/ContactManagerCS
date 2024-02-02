11fa8e5d483b8e5d75d0b69a72b86ca291c18138
AlexShirin <aldorian@mail.ru>
2024-02-02 20:50:55 +0300

    refactoring

--- Api/Database/ContactContext.cs
+++ Api/Database/ContactContext.cs
@@ -19,11 +19,6 @@ public class ContactContext : DbContext
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
-        //modelBuilder.Entity<Contact>().HasData(
-        //    new Contact { Id = 1, Name = "Tom", Email = "a@a.a", Phone = "11", Company = "A" },
-        //    new Contact { Id = 2, Name = "Bob", Email = "b@a.a", Phone = "22", Company = "B" },
-        //    new Contact { Id = 3, Name = "Sam", Email = "c@a.a", Phone = "33", Company = "C" }
-        //);
         modelBuilder.ApplyConfigurationsFromAssembly(typeof(ContactContext).Assembly);
     }
 }
