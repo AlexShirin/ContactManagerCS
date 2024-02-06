ee7eda77ebfb9f1519f99a6d18f89ef33c03a050
AlexShirin <aldorian@mail.ru>
2024-02-06 14:17:13 +0300

    minor bugfix

--- Api/Database/ContactContext.cs
+++ Api/Database/ContactContext.cs
@@ -1,29 +1,15 @@
-﻿using System.IO;
-
-using ContactManagerCS.Models;
+﻿using ContactManagerCS.Models;
 using Microsoft.EntityFrameworkCore;
 
 namespace ContactManagerCS.Database;
 
 public class ContactContext : DbContext
 {
-    public DbSet<Contact> Contact { get; set; } = null!;
+    public DbSet<Contact> Contact { get; set; }
 
     public ContactContext(DbContextOptions<ContactContext> options) : base(options)
     {
         Database.EnsureCreated();
-    }
-
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-    {
-        var configuration = new ConfigurationBuilder()
-            .SetBasePath(AppDomain.CurrentDomain.BaseDirectory)
-            .AddJsonFile("appsettings.json")
-            .Build();
-
-        var connectionString = configuration.GetConnectionString("DefaultConnection");
-
-        optionsBuilder.UseNpgsql(connectionString); 
     }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
